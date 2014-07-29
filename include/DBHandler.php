b88caf415a6ac308630cf9ee2371f8b5e1dd713e
Vishal <vishal.x.patel@nab.com.au>
2014-07-29 16:59:11 +1000

    [TM] Added DataAccess framework and user methods.

--- include/DBHandler.php
+++ include/DBHandler.php
@@ -6,10 +6,13 @@
  * Time: 1:40 PM
  */
 
+require 'medoo.php';
+
 
 class DBHandler {
 
     private  $conn;
+    private  $database;
 
     function __construct()
     {
@@ -18,6 +21,24 @@ class DBHandler {
 
         $db = new DbConnect();
         $this->conn = $db->connect();
+
+
+        $this->database = new medoo([
+            // required
+            'database_type' => 'mysql',
+            'database_name' => 'task_manager',
+            'server' => 'localhost',
+            'username' => 'root',
+            'password' => 'root',
+
+            // optional
+            'port' => 3306,
+            'charset' => 'utf8',
+            // driver_option for connection, read more from http://www.php.net/manual/en/pdo.setattribute.php
+            'option' => [
+                PDO::ATTR_CASE => PDO::CASE_NATURAL
+            ]
+        ]);
     }
 
     /* ------------- `users` table method ------------------ */
@@ -83,7 +104,99 @@ class DBHandler {
         return md5(uniqid(rand(), true));
     }
 
+    public  function  checkLogin($email,$password)
+    {
+        $password_hash = NULL;
+        $stmt = $this->conn->prepare("SELECT password_hash FROM users WHERE email = ?");
+        $stmt->bind_param("s",$email);
+        $stmt->execute();
+        $stmt->bind_result($password_hash);
+        $stmt->store_result();
+
+        if ( $stmt->num_rows > 0)
+        {
+            $stmt->fetch();
+            $stmt->close();
+
+            if ( ($password == $password_hash))
+            {
+                return TRUE;
+            }
+            else
+            {
+                return FALSE;
+            }
+        }
+        else
+        {
+            $stmt->close();
+            return FALSE;
+        }
+
+    }
+
+    public  function  getUserByEmail($email)
+    {
+        $name = NULL;
+        $name = $this->database->get("users","name",["email" => $email]);
+        if ( $name != FALSE )
+        {
+            return $name;
+        }
+        else
+        {
+            return NULL;
+        }
+    }
+
+    public function getApiKeyById($user_id)
+    {
+        $api_key = NULL;
+        $api_key = $this->database->get("users","api_key",["id" => $user_id]);
+        if ( $api_key != FALSE )
+        {
+            return $api_key;
+        }
+        else
+        {
+            return NULL;
+        }
+    }
+
+    public function getUserId($api_key)
+    {
+        $user_id = NULL;
+        $user_id = $this->database->get("users","id",["api_key" => $api_key]);
+        if ( $user_id != FALSE )
+        {
+            return $user_id;
+        }
+        else
+        {
+            return NULL;
+        }
+    }
+
+    public function isValidApiKey($api_key)
+    {
+        $user_id = NULL;
+        $user_id = $this->database->get("users","id",["api_key" => $api_key]);
+        if ( $user_id != FALSE )
+        {
+            return TRUE;
+        }
+        else
+        {
+            return FALSE;
+        }
+    }
+
 }
 
 $handler = new DBHandler();
-$handler->createUser('test.user','test.user@gmail.com','test123');
+//$handler->createUser('test.user','test.user@gmail.com','test123');
+//$handler->checkLogin('test.user@gmail.com','test123');
+//$handler->getUserByEmail('vis8051@gmail.com');
+//$handler->getApiKeyById(1);
+//$handler->getUserId("d3de848e17b474ef2965301d3b9ba817");
+//$handler->isValidApiKey("d3de848e17b474ef2965301d3b9ba817");
