52c348d5675cd29a3a4f5b1a89b188d6595f0259
Vishal <vishal.x.patel@nab.com.au>
2014-07-30 15:16:26 +1000

    [TM] User registration method.

--- v1/index.php
+++ v1/index.php
@@ -3,25 +3,113 @@
 require '../Slim/Slim.php';
 \Slim\Slim::registerAutoloader();
 
-$app = new \Slim\Slim(array(
-    'debug' => true
-));
+$app = new \Slim\Slim();
 
-$app->get('/hello:name', function($name) use ($app) {
-    echo "in get funciton";
-    echo "hello $name";
-});
+// User id from db - Global Variable
+$user_id = NULL;
 
-$app->get('/:name', function($name) use ($app) {
-    echo "in get funciton";
-    echo "hello $name";
-});
+/**
+ * Verifying required params posted or not
+ */
+function verifyRequiredParams($required_fields) {
+    $error = false;
+    $error_fields = "";
+    $request_params = $_REQUEST;
+    // Handling PUT request params
+    echo($_REQUEST);
 
-$app->get('/', function() use ($app) {
-    echo "in get funciton";
-    echo "hello";
+
+    if ($_SERVER['REQUEST_METHOD'] == 'PUT') {
+        $app = \Slim\Slim::getInstance();
+        parse_str($app->request()->getBody(), $request_params);
+    }
+    foreach ($required_fields as $field) {
+        if (!isset($request_params[$field]) || strlen(trim($request_params[$field])) <= 0) {
+            $error = true;
+            $error_fields .= $field . ', ';
+        }
+    }
+
+    if ($error) {
+        // Required field(s) are missing or empty
+        // echo error json and stop the app
+        $response = array();
+        $app = \Slim\Slim::getInstance();
+        $response["error"] = true;
+        $response["message"] = 'Required field(s) ' . substr($error_fields, 0, -2) . ' is missing or empty';
+        echoRespnse(400, $response);
+        $app->stop();
+    }
+}
+
+/**
+ * Validating email address
+ */
+function validateEmail($email) {
+    $app = \Slim\Slim::getInstance();
+    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        $response["error"] = true;
+        $response["message"] = 'Email address is not valid';
+        echoRespnse(400, $response);
+        $app->stop();
+    }
+}
+
+/**
+ * Echoing json response to client
+ * @param String $status_code Http response code
+ * @param Int $response Json response
+ */
+function echoRespnse($status_code, $response) {
+    $app = \Slim\Slim::getInstance();
+    // Http response code
+    $app->status($status_code);
+
+    // setting response content type to json
+    $app->contentType('application/json');
+
+    echo json_encode($response);
+}
+
+/**
+ * User Registration
+ * url - /register
+ * method - POST
+ * params - name, email, password
+ */
+$app->post('/register', function() use ($app) {
+    // check for required params
+    verifyRequiredParams(array('name', 'email', 'password'));
+
+    $response = array();
+
+    // reading post params
+    $name = $app->request->post('name');
+    $email = $app->request->post('email');
+    $password = $app->request->post('password');
+
+    // validating email address
+    validateEmail($email);
+
+    $db = new DbHandler();
+    $res = $db->createUser($name, $email, $password);
+
+    if ($res == USER_CREATED_SUCCESSFULLY) {
+        $response["error"] = false;
+        $response["message"] = "You are successfully registered";
+        echoRespnse(201, $response);
+    } else if ($res == USER_CREATE_FAILED) {
+        $response["error"] = true;
+        $response["message"] = "Oops! An error occurred while registereing";
+        echoRespnse(200, $response);
+    } else if ($res == USER_ALREADY_EXISTED) {
+        $response["error"] = true;
+        $response["message"] = "Sorry, this email already existed";
+        echoRespnse(200, $response);
+    }
 });
 
 $app->run();
+
 ?>
 
